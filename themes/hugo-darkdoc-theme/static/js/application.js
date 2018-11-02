a7b7cc1220f1c6787a6fa256c8dd373c451bb5dd
Christophe Bento <christophe.bento@logdirect.net>
2018-11-02 14:11:44 +0100

    Use navigation button to toggle the menu sidebar on mobile

--- themes/hugo-darkdoc-theme/static/js/application.js
+++ themes/hugo-darkdoc-theme/static/js/application.js
@@ -1,3 +1,8 @@
 $.getJSON( api_url, function( data ) {
   $('#star_count').replaceWith(data["stargazers_count"])
 });
+$(function(){
+  $('.navbar-toggle').on('click', function(){
+    $('.table-of-contents').slideToggle();
+  });
+});
